9cef110f6f32672258d01ba9ba8c0cec9a8920d9
Vaneet Chanana <vaneetchanana98@gmail.com>
2022-09-18 01:14:17 +0530

    Updates

---
+++ precache-manifest.c61701fccce289a021bb32de2fa514e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd9ed9a689eb8852885be93d47bb44a",
+    "url": "/emoji-search/index.html"
+  },
+  {
+    "revision": "3896f89b5a26c3da5987",
+    "url": "/emoji-search/static/css/2.5510c45e.chunk.css"
+  },
+  {
+    "revision": "ebae186629341d97cf71",
+    "url": "/emoji-search/static/css/main.f38963d9.chunk.css"
+  },
+  {
+    "revision": "3896f89b5a26c3da5987",
+    "url": "/emoji-search/static/js/2.4eecd746.chunk.js"
+  },
+  {
+    "revision": "6120b983a03909b7d2cd9bf134169811",
+    "url": "/emoji-search/static/js/2.4eecd746.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebae186629341d97cf71",
+    "url": "/emoji-search/static/js/main.eda95b27.chunk.js"
+  },
+  {
+    "revision": "53f65d824d8bd704bdf5",
+    "url": "/emoji-search/static/js/runtime-main.fcefcbdb.js"
+  }
+]);
